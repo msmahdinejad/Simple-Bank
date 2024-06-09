2a257868c39728a021dcfe1b89dfd7fbd260cbe7
Mohammad Saleh Mahdinejad <msmahdinejad@gmail.com>
2024-06-09 14:30:29 +0330

    login widget back_end compeleted!

--- bank/mainwindow.cpp
+++ bank/mainwindow.cpp
@@ -8,9 +8,89 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     ui->LogInWidget->hide();
     ui->SignUpWidget->hide();
+    QIntValidator *validatorYear = new QIntValidator(1900, 2030, ui->year);
+    ui->year->setValidator(validatorYear);
+    QIntValidator *validatorMonth = new QIntValidator(1, 12, ui->month);
+    ui->month->setValidator(validatorMonth);
+    QIntValidator *validatorDay = new QIntValidator(1, 30, ui->day);
+    ui->day->setValidator(validatorDay);
+    QIntValidator *validatorNationalCode = new QIntValidator(ui->nationalCode);
+    ui->nationalCode->setValidator(validatorNationalCode);
+    DB = QSqlDatabase::addDatabase("QSQLITE");
+    DB.setDatabaseName("C:/saleh/UNI/AP/Bank/uiap-sec-mini-project-msmahdinejad/Data.db");
+    if(!DB.open())
+        qDebug() << "DB in first page failed!";
+    else
+        qDebug() << "DB in first page opened!";
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_MainLoginButton_clicked()
+{
+    ui->LogInWidget->show();
+}
+
+
+void MainWindow::on_MainSignupButton_clicked()
+{
+    ui->SignUpWidget->show();
+}
+
+
+
+void MainWindow::on_LogInInSignUp_clicked()
+{
+    ui->LogInWidget->show();
+}
+
+
+void MainWindow::on_SignUpnInLogIn_clicked()
+{
+    ui->SignUpWidget->show();
+}
+
+bool MainWindow::checkLoginInputs()
+{
+    QString username, password;
+    username = ui->usernameInLogIn->text();
+    password = ui->PasswordInLogin->text();
+    if(username.isEmpty() || password.isEmpty())
+    {
+        QMessageBox::critical(this, "Error", "Fill in all the items!");
+        return false;
+    }
+    QSqlQuery query;
+    query.prepare("SELECT * FROM users WHERE username ='" + username + "'");
+    if (!query.exec())
+    {
+        QMessageBox::critical(this, "Error", query.lastError().text());
+        return false;
+    }
+    QSqlRecord record = query.record();
+    if (!query.next())
+    {
+        QMessageBox::critical(this, "Error", "Username not found!");
+        return false;
+    }
+    QString DBUsername = record.value("username").toString();
+    QString DBPassword = record.value("password").toString();
+    if(password != DBPassword)
+    {
+        QMessageBox::critical(this, "Error", "Password is not correct");
+        return false;
+    }
+    return true;
+}
+
+void MainWindow::on_LoginButton_clicked()
+{
+    if(checkLoginInputs())
+    {
+
+    }
+}
+
--- bank/mainwindow.h
+++ bank/mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QIntValidator>
+#include <QMessageBox>
+#include <QDebug>
+#include <QtSql>
+#include <QFileInfo>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +22,22 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_MainLoginButton_clicked();
+
+    void on_MainSignupButton_clicked();
+
+    void on_LogInInSignUp_clicked();
+
+    void on_SignUpnInLogIn_clicked();
+
+    void on_LoginButton_clicked();
+
+private:
+    bool checkLoginInputs();
+
 private:
     Ui::MainWindow *ui;
+    QSqlDatabase DB;
 };
 #endif // MAINWINDOW_H
